015ae6e815ba1d507f1e6ec25447fe93cc5d5d41
Seiki Ishimaru <melodian@saturn.dti.ne.jp>
2018-12-30 21:07:58 +0900

    config vendor publish

--- src/LaravelServiceProvider.php
+++ src/LaravelServiceProvider.php
@@ -15,11 +15,22 @@ class LaravelServiceProvider extends ServiceProvider
     public function register()
     {
         $this->commands(ClearCache::class);
+        $this->registerConfigPublish();
 
         $this->app->singleton(Cache::class, function () {
             $instance = new Cache($this->app->make('files'));
 
             return $instance->setContainer($this->app);
         });
+    }
+
+    public function registerConfigPublish() {
+        $configPath = __DIR__ . '/config/page-cache.php';
+        if (function_exists('config_path')) {
+            $publishPath = config_path('page-cache.php');
+        } else {
+            $publishPath = base_path('config/page-cache.php');
+        }
+        $this->publishes([$configPath => $publishPath],'config');
     }
 }
